752bb3b60ab21d074392ffb6bdf12445784d29ab
dbc-warrior <apprentice@devbootcamp.com>
2013-09-11 11:40:58 -0700

    add .takers alias as has_many_through association with options for class_name and source and foreign_key

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,9 +1,17 @@
 class Survey < ActiveRecord::Base
   has_many :completions
-  #also has many users through completions
+
+  has_many :takers, 
+           :through => :completions, 
+           :class_name => "User",
+           :foreign_key => "user_id", 
+           :source => :user
+
   has_many :questions
   
-  belongs_to :creator, class_name: "User", :foreign_key => "user_id"
+  belongs_to :creator, 
+             :class_name => "User", 
+             :foreign_key => "user_id"
   
   validates :title, uniqueness: true
 
