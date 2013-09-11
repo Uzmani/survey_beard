76052f7b3a6b09ac8605a6811853deabdfc5ec15
dbc-warrior <apprentice@devbootcamp.com>
2013-09-11 12:10:03 -0700

    formatting changes

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,16 +2,16 @@ class Survey < ActiveRecord::Base
   has_many :completions
 
   has_many :takers, 
-           :through => :completions, 
-           :class_name => "User",
-           :foreign_key => "user_id", # confused why we need line 6 and 7 based on http://apidock.com/rails/ActiveRecord/Associations/ClassMethods/has_many
-           :source => :user
+             :through => :completions, 
+             :class_name => "User",
+             :foreign_key => "user_id", # confused why we need line 6 and 7 based on http://apidock.com/rails/ActiveRecord/Associations/ClassMethods/has_many
+             :source => :user
 
   has_many :questions
   
   belongs_to :creator, 
-             :class_name => "User", 
-             :foreign_key => "user_id"
+               :class_name => "User", 
+               :foreign_key => "user_id"
   
   validates :title, uniqueness: true
 
