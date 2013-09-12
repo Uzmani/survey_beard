84454286578afecafe2b3abd393a1e9a2ec5c518
dbc-warrior <apprentice@devbootcamp.com>
2013-09-11 17:23:41 -0700

    refactor sign-up validations -- extract render() function and iterate over errors array

--- public/js/validations.js
+++ public/js/validations.js
@@ -1,50 +1,56 @@
-$(function(){
-  $('#signup-form').on('submit', function(e){
-    $('#invalid-errors').empty();
-
-    var errors = [];
-    var nameInput = $('#signup-name[name="name"]').val();
-    var emailInput = $('#signup-email[name="email"]').val();
-    var passwordInput = $('#signup-password[name="password"]').val();
-
-    namePresent(nameInput, errors);
-    validateEmail(emailInput, errors);
-    passwordHasNumber(passwordInput, errors);
-    validatePasswordLength(passwordInput, errors);
-    console.log(errors);
-    if (errors.length > 0){
-      e.preventDefault();
-      // iterate over the array of error messages and append each one into the errors div
+var namePresent = function (name) {
+    if (name.length < 1) {
+        // ^ pull out this append method from each validation and just store the full error message in the array
+        return "Must have a name"; // why don't the messages match?
     }
-  });  
+};
+
+var validateEmail = function (email) {
+    if ((/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/).test(email) === false) {
+        return "Must be valid email";
+    }
+};
+
+var passwordHasNumber = function (password) {
+    if ((/\d/).test(password) === false) {
+        return "Password should have number";
+    }
+};
+
+var validatePasswordLength = function (password) {
+    if (password.length <= 6) {
+        return "Password should have at least 6 characters";
+    }
+};
+
+function renderErrors(functions) {
+    var errors = []
+    for (i = 0; i < functions.length; i++) {
+        if (functions[i]){ 
+          errors.push(functions[i])
+        }
+    }
+    return errors
+}
+
+$(function () {
+    $('#signup-form').on('submit', function (e) {
+        $('#invalid-errors').empty();
+
+        var nameInput = $('#signup-name[name="name"]').val();
+        var emailInput = $('#signup-email[name="email"]').val();
+        var passwordInput = $('#signup-password[name="password"]').val();
+        var errors = renderErrors(
+        [namePresent(nameInput),
+        validateEmail(emailInput),
+        passwordHasNumber(passwordInput),
+        validatePasswordLength(passwordInput)])
+
+        if (errors.length > 0) {
+            e.preventDefault();
+            for (i = 0; i < errors.length; i++) {
+                $('#invalid-errors').append("<li class='signup-error'>" + errors[i] + "</li>")
+            }
+        }
+    });
 });
-
-
-var namePresent = function(name, s) {
-  if (name.length < 1) {
-    $('#invalid-errors').append("<li class='signup-error'>Must enter your name</li>");
-    // ^ pull out this append method from each validation and just store the full error message in the array
-    s.push("Must have a name"); // why don't the messages match?
-  }
-};
-
-var validateEmail = function(email, s) {
-  if ((/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/).test(email) === false){
-    $("#invalid-errors").append("<li class='signup-error'>Must be a valid email address</li>");
-    s.push("Must be valid email");
-  }
-};
-
-var passwordHasNumber = function(password, s) {
-  if ((/\d/).test(password) === false){
-    $('#invalid-errors').append("<li class='signup-error'>Password must have at least one number</li>");
-    s.push("Password should have number");
-  }
-};
-
-var validatePasswordLength = function(password, s) {
-  if (password.length <= 6){
-    $('#invalid-errors').append("<li class='signup-error'>Password must be at least 6 characters long</li>");
-    s.push("Password should have at least 6 characters");
-  }
-};
